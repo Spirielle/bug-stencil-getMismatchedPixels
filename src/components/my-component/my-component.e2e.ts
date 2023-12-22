626f714d35c0d0bce17bf5722b0026fecbda29e4
clmclaughlin <clmclaughlin@genetec.com>
2023-12-22 17:39:53 -0500

    added test case for when no clipping is set

--- src/components/my-component/my-component.e2e.ts
+++ src/components/my-component/my-component.e2e.ts
@@ -1,7 +1,7 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('my-component', () => {
-  it('looks the same', async () => {
+  it('looks the same when clipped to 800 x 800', async () => {
     const page = await newE2EPage();
 
     // REPRO STEPS
@@ -47,5 +47,29 @@ describe('my-component', () => {
 
     console.log(results);
     // Result: Test passed, screenshots compared: 1
+  });
+
+  it('looks the same when not clipped', async () => {
+    const page = await newE2EPage();
+
+    // REPRO STEPS - HAPPY PATH
+    // STEP 1. Run yarn install
+    // STEP 2. Run yarn run build
+    // STEP 3. Run yarn test
+
+    // STEP 4. Comment this line vvvvvvvv  (line 61)
+    await page.setContent('<my-component></my-component><div>Hello World</div>');
+
+    // STEP 5. Uncomment this line vvvvvvv  (line 64)
+    // await page.setContent('<my-component></my-component><div>Hello World, with visual difference</div>');
+
+
+    // STEP 6. Run yarn test
+
+    const results = await page.compareScreenshot();
+    expect(results).toMatchScreenshot();
+
+    console.log(results);
+    // Result: "Error: Image data size does not match width/height."
   });
 });
